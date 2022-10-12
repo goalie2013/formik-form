126454527d4602c708d811053b5a0ffc91219ff9
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-10-11 22:21:12 -0700

    firebase file in top directory

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBilXh8307q-BwWjU5exFJ908ISl8d16Rs",
+  authDomain: "formik-form-ci-cd.firebaseapp.com",
+  projectId: "formik-form-ci-cd",
+  storageBucket: "formik-form-ci-cd.appspot.com",
+  messagingSenderId: "803353913344",
+  appId: "1:803353913344:web:964958e33d86ef0358d0e5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
